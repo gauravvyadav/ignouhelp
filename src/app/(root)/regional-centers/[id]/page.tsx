ab48ce6686cdcb0c101182f38055bcd89b3b8d57
Gaurav yadav <gauravnyadav6@gmail.com>
2024-02-25 17:47:27 +0530

    feat(rc): center details done

--- src/app/(root)/regional-centers/[id]/page.tsx
+++ src/app/(root)/regional-centers/[id]/page.tsx
@@ -1,20 +1,126 @@
+import Link from "next/link"
 import { centerDetails } from "@/src/data/regional-network"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/src/components/ui/tooltip"
 
 export default function RegionalCenterDetails({
   params,
 }: {
   params: { id: string }
 }) {
-  console.log(centerDetails.length)
-
   return (
-    <div className="w-full bg-blue-200">
-      <h1 className="text-3xl font-bold">Regional Center Details</h1>
-      <p>
-        {centerDetails[params.id]?.districts.map((districts) => (
-          <div key={districts}>{districts}</div>
-        ))}
-      </p>
+    <div className="mt-8 w-full max-w-[600px] ">
+      <TooltipProvider>
+        {" "}
+        <h2 className="text-3xl font-semibold capitalize">
+          {centerDetails[params.id].rc_name}
+        </h2>
+        <table className="mt-4 capitalize">
+          <tbody>
+            <tr>
+              <td className="pr-4 align-baseline font-semibold">
+                <div className="text-nowrap pr-4">RC Code:</div>
+              </td>
+              <td className="flex flex-wrap pb-4">
+                <div className="rounded-md bg-secondary p-1 px-2">
+                  {centerDetails[params.id].rc_code}
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="pr-4 align-baseline font-semibold">
+                <div className="pr-4">Address:</div>
+              </td>
+              <td className="pb-4">
+                <div className="rounded-md bg-secondary p-1 px-2">
+                  {centerDetails[params.id].address}
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="pr-4 align-baseline font-semibold">
+                <div className="pr-4">Email:</div>
+              </td>
+              <td className="flex flex-wrap gap-2 pb-4 lowercase">
+                {centerDetails[params.id].email.split(",").map((email) => (
+                  <div className="rounded-md bg-secondary p-1 px-2" key={email}>
+                    <Link href={`mailto:${email}`}>{email}</Link>
+                  </div>
+                ))}
+              </td>
+            </tr>
+            <tr>
+              <td className="pr-4 align-baseline font-semibold">
+                <div className="pr-4">Phone:</div>
+              </td>
+              <td className="flex flex-wrap gap-2 pb-4">
+                {centerDetails[params.id].phone.map((no) => (
+                  <div className="rounded-md bg-secondary p-1 px-2" key={no}>
+                    {no}
+                  </div>
+                ))}
+              </td>
+            </tr>
+            <tr>
+              <td className="pr-4 align-baseline font-semibold">
+                <div className="pr-4">State:</div>
+              </td>
+              <td className="flex flex-wrap gap-2 pb-4">
+                {centerDetails[params.id].state.split(",").map((s) => (
+                  <div
+                    className="text-nowrap rounded-md bg-secondary p-1 px-2"
+                    key={s}
+                  >
+                    {s}
+                  </div>
+                ))}
+              </td>
+            </tr>
+            <tr>
+              <td className="pr-4 align-baseline font-semibold">
+                <div className="pr-4">
+                  Districts:
+                  <br /> (Operational Area):
+                </div>
+              </td>
+              <td className="flex flex-wrap gap-2 pb-4">
+                {centerDetails[params.id].districts.map((d) => (
+                  <div
+                    className="text-nowrap rounded-md bg-secondary p-1 px-2"
+                    key={d}
+                  >
+                    {d}
+                  </div>
+                ))}
+              </td>
+            </tr>
+            <tr>
+              <td className="pr-4 align-baseline font-semibold">
+                <div className="text-nowrap pr-4">Website:</div>
+              </td>
+              <td className="flex flex-wrap pb-4">
+                <Tooltip>
+                  <TooltipTrigger>
+                    <div className="rounded-md bg-secondary p-1 px-2 lowercase">
+                      <Link
+                        href={centerDetails[params.id].website}
+                        target="_blank"
+                      >
+                        {centerDetails[params.id].website}
+                      </Link>
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent>Click to Open</TooltipContent>
+                </Tooltip>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </TooltipProvider>
     </div>
   )
 }
--- src/components/common/theme-switch.tsx
+++ src/components/common/theme-switch.tsx
@@ -1,21 +1,31 @@
 "use client"
 
+import { Fragment } from "react"
 import { useTheme } from "next-themes"
 import { FiMoon, FiSun } from "react-icons/fi"
 import { Button } from "@/src/components/ui/button"
 
 export default function ThemeSwitch() {
   const { theme, setTheme } = useTheme()
-  const isDark = theme === "dark"
 
   return (
-    <Button
-      variant="ghost"
-      size={"icon"}
-      className="h-8 w-8"
-      onClick={() => setTheme(isDark ? "light" : "dark")}
-    >
-      {isDark ? <FiMoon size={18} /> : <FiSun size={18} />}
-    </Button>
+    <Fragment>
+      <Button
+        variant="ghost"
+        size={"icon"}
+        className="h-8 w-8 dark:hidden"
+        onClick={() => setTheme("dark")}
+      >
+        <FiSun className="h-4 w-4" />
+      </Button>
+      <Button
+        variant="ghost"
+        size={"icon"}
+        className="hidden h-8 w-8 dark:flex"
+        onClick={() => setTheme("light")}
+      >
+        <FiMoon className="h-4 w-4" />
+      </Button>
+    </Fragment>
   )
 }
